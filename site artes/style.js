463e756e371c766985840eae42f9244c3a68cef5
João Pedro <jpfo175@gmail.com>
2021-03-18 11:02:33 -0300

    Mascaras para formularios em HTML usando Java Script

---
+++ site artes/style.js
@@ -0,0 +1,9 @@
+document.getElementById("botao1").textContent = "Home";
+document.getElementById("botao2").textContent = "Queijo";
+document.getElementById("botao3").textContent = "Goiaba";
+document.getElementById("botao4").textContent = "Surucucu";
+document.getElementById("email").placeholder = "Digita o email ae irmão";
+document.getElementById("senha").placeholder = "Senha tbm po";
+document.getElementById("div1").style.borderColor = "yellow";
+document.getElementById("div1").style.borderRadius = "40px";
+document.getElementById("navbar").style.backgroundColor = "blue";
